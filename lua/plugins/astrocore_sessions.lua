ba8539866b5931aaada14dcafe826bea4afcce8d
Eric Jacobs <ejacobs@doximity.com>
2025-02-04 19:52:40 -0700

    wip update

--- lua/plugins/astrocore_sessions.lua
+++ lua/plugins/astrocore_sessions.lua
@@ -2,18 +2,9 @@ return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-    mappings = {
-      n = {
-        ["<Leader>fg"] = {
-          function()
-            require("telescope").extensions.live_grep_args.live_grep_args()
-          end,
-          desc = "Telescope live-grep with args"
-        },
-      },
-    },
+    -- Configuration table of session options for AstroNvim's session management powered by Resession
     sessions = {
+      -- Configure auto saving
       autosave = {
         last = true, -- auto save last session
         cwd = true, -- auto save session for each working directory
--- lua/plugins/stay-centered.lua
+++ lua/plugins/stay-centered.lua
@@ -1,6 +1,14 @@
 return {
   {
     "arnamak/stay-centered.nvim",
-    enabled = true,
+    config = function()
+      require('stay-centered').setup({
+        -- The filetype is determined by the vim filetype, not the file extension. In order to get the filetype, open a file and run the command:
+        -- :lua print(vim.bo.filetype)
+        skip_filetypes = {},
+        -- Set to false to disable by default
+        enabled = false,
+      })
+    end
   }
 }
